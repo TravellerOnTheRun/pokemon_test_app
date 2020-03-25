754ab578dbbfc4ee101ae97e408f1556c774baae
Alexander <alexivchenko9@gmail.com>
2020-03-25 14:16:35 +0700

    First commit + app rough structure

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div>
+            <h2>Cardlist component</h2>
+            <ul>
+                <li>Card</li>
+                <li>Card2</li>
+                <li>Card3</li>
+                <li>Card4</li>
+            </ul>
+        </div>
+    );
+};
