211dc13610445528aa8d99e03332868d4aaf4a48
TravellerOnTheRun <47615361+TravellerOnTheRun@users.noreply.github.com>
2020-03-25 18:27:07 +0700

    Did the main page layout style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Button, Pagination } from 'antd';
 
 import Menu from './components/Menu/Menu';
 import CardList from './components/CardList/CardList';
-import Pagination from './components/Pagination/Pagination';
+// import Pagination from './components/Pagination/Pagination';
 
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Find Your Pokemon</h1>
-        <button>burger icon</button>
+        <Button className='button-menu'>
+          <span className='one_dash'></span>
+          <span className='two_dash'></span>
+          <span className='three_dash'></span>
+        </Button>
       </header>
       {/*<Menu />*/}
       <CardList />
-      <Pagination />
+      <Pagination className='pagination' defaultCurrent={1} total={50} />
     </div>
   );
 }
