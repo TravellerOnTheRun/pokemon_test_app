88bc45978ea270df0de3461acdefd27dbaef7a75
Alexander <alexivchenko9@gmail.com>
2020-03-31 13:40:20 +0700

    Added Form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Login.css';
-import { Input, Button } from 'antd';
+import { Form, Input, Button } from 'antd';
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+};
 
 export default props => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signupHandler = (email, password) => {
+        console.log(email, password);
+    };
+
+    const onFinish = values => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div className='layer-login' onClick={props.dismissLogin}>
-            <div className='login-component'>
-                <Input className='login_input' placeholder="Your login" />
-                <Input className='password_input' placeholder="Your password" />
-                <Button className='login_btn' type="primary">Log In</Button>
-                <h4>Continue without login</h4>
-            </div>
+        <div className='login-component'>
+            <div className='layer-login' onClick={props.dismissLogin}></div>
+            <Form
+                {...layout}
+                name="basic"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input className='login_input' placeholder="Your login" />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input className='password_input' placeholder="Your password" />
+                </Form.Item>
+
+                <Form.Item {...tailLayout}>
+                    <Button className='login_btn' type="primary">Log In</Button>
+                </Form.Item>
+            </Form>
+            <h4 onClick={props.dismissLogin} >Continue without login</h4>
         </div>
     );
 };
